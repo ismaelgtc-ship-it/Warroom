fec7211779378a137ea93f7f4788bea3fbbe92a6
GOGETA <ismael.gtc@gmail.com>
2026-02-21 17:29:28 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // Cloudflare Pages serves this as static file.
 // Set to your Overseer Render URL (public).
 window.WARROOM_CONFIG = {
-  OVERSEER_PUBLIC_URL: "https://overseer-abc123.onrender.com"
+  OVERSEER_PUBLIC_URL: "https://overseer-12w1.onrender.com"
 };
